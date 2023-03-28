89c04e079cbc2a606a866d899e9ab19febdf7181
Philipp <philipp.liehm@gmail.com>
2023-03-28 08:20:13 +0200

    wie  vorher

--- MiniGames/3_DDI/test_3_Reaktion.go
+++ MiniGames/3_DDI/test_3_Reaktion.go
@@ -20,11 +20,11 @@ func TesteTaste(taste uint16) {
 	case Taste:
 		fmt.Println("Richtig! Das haben Sie aber toll geloest!!")
 		gfx.SchreibeFont(200, 500, "Richtig! Das haben Sie aber toll geloest!!")
-		gfx.LadeBildMitColorKey(650, 50, "LWB-A-BMP/WtheK_black.bmp", 255, 255, 255)
+		gfx.LadeBildMitColorKey(650, 50, "../../Bilder/WtheK_black.bmp", 255, 255, 255)
 	default:
 		fmt.Println("Ohjeee, das ist ja falsch, wie kommen Sie de6nn darauf???")
 		gfx.SchreibeFont(200, 500, "Ohjeee, das ist ja falsch, wie kommen Sie denn darauf???")
-		gfx.LadeBildMitColorKey(650, 50, "LWB-A-BMP/WtheK_black_sad.bmp", 255, 255, 255)
+		gfx.LadeBildMitColorKey(650, 50, "../../Bilder/WtheK_black_sad.bmp", 255, 255, 255)
 	}
 	//time.Sleep(1e9)
 }
@@ -38,7 +38,7 @@ func LoseTaste() uint16 {
 func main() {
 
 	gfx.Fenster(breite, höhe)
-	gfx.SetzeFont("Ubuntu-B.ttf", 20)
+	gfx.SetzeFont("../../Schriftarten/Ubuntu-B.ttf", 20)
 
 A:
 	for {
@@ -50,7 +50,7 @@ A:
 		gfx.Rechteck(100, 100, 400, 200)
 		Taste = LoseTaste()
 		gfx.SchreibeFont(150, 150, "Druecke "+string(Taste))
-		gfx.LadeBildMitColorKey(650, 50, "LWB-A-BMP/WtheK_black.bmp", 255, 255, 255)
+		gfx.LadeBildMitColorKey(650, 50, "../../Bilder/WtheK_black.bmp", 255, 255, 255)
 		//gfx.LadeBildMitColorKey(50, 400, "LWB-A-BMP/3_Fachdidaktik_Planung.bmp", 255, 255, 255)
 		fmt.Println("Rechteck")
 		gfx.UpdateAn()
