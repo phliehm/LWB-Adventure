3d8ac12ed9e7c081863aad86c76f3d8705e4378b
cyrianna <125969887+cyrianna@users.noreply.github.com>
2023-04-25 08:25:01 +0200

    neue Raum-Bilder, neue Klasse "Vierecke" und mainfloor-Programm

---
+++ Klassen/vierecke/viereckeimpl.go
@@ -0,0 +1,172 @@
+package vierecke
+
+// Annalena Cyriacus
+// 24.04.2023
+
+import ( "gfx" ; "fmt" ; "unsafe")
+
+// Datenstruktur eines Vierecks
+// ----------------------------
+
+type data struct {
+	xA, yA uint16
+	xB, yB uint16 
+	xC, yC uint16
+	xD, yD uint16
+	r, g, b uint8 //optional
+}
+
+// Methoden eines Dreiecks
+// -----------------------
+
+// Vor.: -
+// Erg.: Ein Viereck mit den Koordinaten (xA,yA), (xB,yB), (xC,yC) und
+//       (xD,yD) ist geliefert. A ist dabei die obere linke Ecke.
+func New (xA,yA,xB,yB,xC,yC,xD,yD uint16) *data { // *data erfüllt das Interface Viereck
+	var v *data
+	v = new(data)
+	v.xA = xA
+	v.yA = yA
+	v.xB = xB
+	v.yB = yB
+	v.xC = xC
+	v.yC = yC
+	v.xD = xD
+	v.yD = yD
+	return v
+}
+
+
+// Vor.: -
+// Erg.: Alle Koordinaten der vier Ecken des Vierecks sind geliefert.
+func (v *data) Get () (xA,yA,xB,yB,xC,yC,xD,yD uint16) {
+	return v.xA,v.yA,v.xB,v.yB,v.xC,v.yC,v.xD,v.yD
+}
+
+// Vor.: -
+// Erg.: Das Viereck hat die eingegebenen Koordinaten.
+func (v *data) Set (xA,yA,xB,yB,xC,yC,xD,yD uint16) () {
+	v.xA = xA
+	v.yA = yA
+	v.xB = xB
+	v.yB = yB
+	v.xC = xC
+	v.yC = yC
+	v.xD = xD
+	v.yD = yD
+}
+
+func (v *data) GibKoordA() (uint16,uint16) {
+	return v.xA, v.yA
+}
+
+func (v *data) GibKoordB() (uint16,uint16) {
+	return v.xB, v.yB
+}
+
+func (v *data) GibKoordC() (uint16,uint16) {
+	return v.xC, v.yC
+}
+
+func (v *data) GibKoordD() (uint16,uint16) {
+	return v.xD, v.yD
+}
+
+func (v *data) SetzeKoordA(x,y uint16) {
+	if x < 1200 && y < 700 {
+		v.xA = x
+		v.yA = y
+	}
+}
+		
+func (v *data) SetzeKoordB(x,y uint16) {
+	if x < 1200 && y < 700 {
+		v.xB = x
+		v.yB = y
+	}
+}
+
+func (v *data) SetzeKoordC(x,y uint16) {
+	if x < 1200 && y < 700 {
+		v.xC = x
+		v.yC = y
+	}
+}
+
+func (v *data) SetzeKoordD(x,y uint16) {
+	if x < 1200 && y < 700 {
+		v.xD = x
+		v.yD = y
+	}
+}
+
+/*		
+func (v *data) GibFarbe() (uint8,uint8,uint8) {
+	return v.r, v.g, v.b
+}
+
+func (v *data) SetzeFarbe (r,g,b uint8) {
+	v.r = r
+	v.g = g
+	v.b = b
+}
+
+*/
+
+// Vor.: -
+// Erg.: Eine Repräsentation des Vierecks in Form eines Strings ist geliefert.
+func (v *data) String () string {
+	return "Viereck mit den Koordinaten A("+fmt.Sprint(v.xA)+";"+fmt.Sprint(v.yA)+"), B("+fmt.Sprint(v.xB)+";"+fmt.Sprint(v.yB)+"), C("+fmt.Sprint(v.xC)+";"+fmt.Sprint(v.yC)+") und D("+fmt.Sprint(v.xD)+";"+fmt.Sprint(v.yD)+")"
+}
+	
+	
+// Vor.: -
+// Erg.: Ein stets gleichlanger Bytestrom, der die serialisierte Form
+//       des Vierecks darstellt, ist geliefert.
+func (v *data) Kodieren () (b []byte) {
+	b = (*(*[unsafe.Sizeof(*v)]byte)(unsafe.Pointer(&(*v))))[:] // Es wird rüberkopiert!
+	return
+}
+	
+
+// Vor.: -
+// Eff.: Das Viereck hat jetzt die Eigenschaften, die im Bytestrom
+//       enthalten waren. Seine vorher vorhandenen Eigenschaften
+//       gibt es nicht mehr.
+func (v *data) Dekodieren (b []byte) {
+	(*v) = *(*data)(unsafe.Pointer(&b[0])) 
+}
+	
+// Vor.: -
+// Erg.: Eine tiefe Kopie des Vierecks v in Form einer Variablen des
+//       Typs interface {} ist geliefert. Die Objekte v und 
+//       v.Kopie().(Viereck) sind unterschiedliche Objekte, haben aber
+//       komplett identische Eigenschaften.
+func (v * data) Kopie () interface {} {
+	var kopie *data = new(data) // 1. Erzeuge eine Kopie-Instanz!
+	*kopie = *v // Der Inhalt des Structs von kopie sei bitte mit den Werten des Inhalts des Structs von v belegt.
+	return kopie
+}
+
+// Vor.: -
+// Erg.: Das Viereck ist in der aktuellen Stiftfarbe gezeichnet.
+func (v *data) Zeichnen() {
+	gfx.Stiftfarbe(v.r, v.g, v.b)
+	gfx.Linie(v.xA, v.yA, v.xB, v.yB)
+	gfx.Linie(v.xB, v.yB, v.xC, v.yC)
+	gfx.Linie(v.xC, v.yC, v.xD, v.yD)
+	gfx.Linie(v.xD, v.yD, v.xA, v.yA)
+}
+
+func (v *data) Angeklickt(x,y uint16) (bool) {						// Checkt, ob angeklickt
+	
+	var min, max func(a,b uint16) uint16
+	min = func (a,b uint16) uint16 {
+		if a < b {return a} else {return b}
+	}
+	max = func (a,b uint16) uint16 {
+		if a > b {return a} else {return b}
+	}
+	
+	return min(v.xA,v.xB) <= x && x <= max(v.xC,v.xD) && min(v.yA,v.yD) <= y && y <= max(v.yB,v.yC)
+}
---
+++ lwb-adventure.go
@@ -0,0 +1,72 @@
+package main
+
+// Annalena Cyriacus
+// 24.04.2023
+
+import . "gfx"
+import "./Klassen/vierecke"
+import "./Klassen/objekte"
+
+
+func maussteuerung (tueren *[]vierecke.Viereck, maus objekte.Objekt) {
+	//var taste uint8
+	//var status int8
+	for {
+		taste, status, mausX, mausY := MausLesen1()
+		
+		maus.SetzeKoordinaten(mausX,mausY)				// Aktualisiert Maus-Koordinaten
+		
+		if taste==1 && status==1 { 						//LINKE Maustaste gerade gedrückt
+			for i,tuer := range *tueren { 				// Zeichnet alleweiteren Objekte ein
+				if tuer.Angeklickt(mausX,mausY) {
+						//TO DO
+						switch i {
+							case 0:
+							LadeBild(0,50,"./Bilder/Seminarraum_1.bmp")
+							case 1:
+							LadeBild(0,50,"./Bilder/Seminarraum_2.bmp")
+							case 2:
+							LadeBild(0,50,"./Bilder/Seminarraum_3.bmp")
+							case 3:
+							LadeBild(0,50,"./Bilder/Seminarraum_4.bmp")
+							case 4:
+							LadeBild(0,50,"./Bilder/Tür5.bmp")
+						}
+				}
+			}
+		}
+		/*
+		if taste == 3 && status == 1 { 			//RECHTE Maustaste gerade gedrückt
+			//TO DO
+		}
+		*/
+	}
+}
+
+
+func main () {
+	Fenster(1200,700)
+	LadeBild(0,50,"./Bilder/mainfloor.bmp")
+	
+	var maus objekte.Objekt
+	maus = objekte.New(0,0,0,0)						// Erstellt das Objekt MAUSZEIGER
+	tueren := make([]vierecke.Viereck,0)			// Array für die Türen des Mainfloors
+	var tuer1, tuer2, tuer3, tuer4, tuer5 vierecke.Viereck
+	tuer1 = vierecke.New(45,235,40,595,220,595,220,235)
+	tuer2 = vierecke.New(935,290,935,520,1010,545,1020,255)
+	tuer3 = vierecke.New(425,330,430,470,465,460,460,350)
+	tuer4 = vierecke.New(720,355,710,455,740,460,750,340)
+	tuer5 = vierecke.New(570,350,570,435,625,435,625,350)
+	Stiftfarbe(0,0,0)
+	//tuer1.Zeichnen()
+	//tuer2.Zeichnen()
+	//tuer3.Zeichnen()
+	//tuer4.Zeichnen()
+	//tuer5.Zeichnen()
+	tueren = append(tueren,tuer1,tuer2,tuer3,tuer4,tuer5)
+	
+	maussteuerung(&tueren,maus)
+	
+	
+	TastaturLesen1()
+}
