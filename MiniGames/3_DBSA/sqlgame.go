e198cdf5614277af380bff1afa0aeea1489b5cac
cyrianna <125969887+cyrianna@users.noreply.github.com>
2023-06-04 18:51:46 +0200

    Update sqlgame.go

--- MiniGames/3_DBSA/sqlgame.go
+++ MiniGames/3_DBSA/sqlgame.go
@@ -15,17 +15,19 @@ import (
 var path string = ""
 var path2 string = "./" //MiniGames/2_ALP2/"
 
+var texte []string = make([]string,14)
+var aktuellerText int = 1
+
 //----------------------Hilfsfunktionen----------------------------
 
-func angeklickt(button buttons.Button) bool {
-	taste, status, mausX, mausY := MausLesen1()
-	if taste==1 && status==1 {
-		if button.TesteXYPosInButton(mausX,mausY) {
-			return true
-		}
-	}
-	return false
+func bubbleTexte() {
+	texte[0] = "\n\nUm die DBSA-Prüfung zu bestehen,\nmusst Du Dein SQL-Wissen nutzen,\num alle Geheimnisse der LWB-Welt\nzu enthüllen!"
+	texte[1] = "Aufgabe 1"
+	texte[2] = "Aufgabe 2"
+	texte[3] = "Aufgabe 3"
+	texte[4] = "Aufgabe 4"
 }
+
 
 //func SQLgame() (float32,uint32) {
 func SQLgame() {
@@ -33,9 +35,11 @@ func SQLgame() {
 //-----------------initialisiere gfx-Fenster-----------------------	
 	Fenster(1200,700)
 	SetzeFont(path2 + "Schriftarten/terminus-font/TerminusTTF-Bold-4.49.2.ttf",22)
+	bubbleTexte()
 	
 //------------------Variablen--------------------------------------
-	var weiter buttons.Button = buttons.New(445,240,80,35,0,255,0,true,"weiter")
+	var next buttons.Button = buttons.New(445,240,80,35,0,255,0,true,"   next")
+	var firsted textboxen.Textbox = textboxen.New(320,445,820,208)
 	var datainfo textboxen.Textbox = textboxen.New(600,270,540,70)
 	var ausgabe textboxen.Textbox = textboxen.New(320,375,820,45)
 	SetzeFont(path2 + "Schriftarten/Ubuntu-B.ttf",50)
@@ -49,15 +53,12 @@ func SQLgame() {
 	
 	LadeBild(150,90,path2 + "Bilder/bubble2_flipped_400.bmp")
 	LadeBildMitColorKey(50,400,path2 + "Bilder/Herk_200.bmp",255,255,255)
-	
+		
 	SetzeFont(path2 + "Schriftarten/brlnsdb.ttf",60)
 	Stiftfarbe(0,255,0)
 	SchreibeFont(140,10,"SQL-Quest:")
 	SetzeFont(path2 + "Schriftarten/brlnsr.ttf",50)
 	SchreibeFont(460,20,"Explore the world of LWB!")
-	
-	weiter.ZeichneButton()
-	weiter.SetzeFont(path2 + "Schriftarten/terminus-font/TerminusTTF-4.49.2.ttf")
 	
 	//infotext.SetzeFarbe(0,0,0)
 	infotext.SetzeZeilenAbstand(5)
@@ -81,30 +82,55 @@ func SQLgame() {
 	ausgabe.SchreibeText("Hier erscheint das Ergebnis Deiner SQL-Anfrage, sobald Du sie eingegeben und mit ENTER ausgeführt hast!")
 	ausgabe.Zeichne()
 	
+	firsted.HintergrundAn(true)
+	firsted.SetzeHintergrundFarbe(0,0,0)
+	firsted.SetzeFarbe(255,255,255)
+	firsted.SchreibeText("SELECT ...  <-- Gib Deine SQL-Anfrage hier ein und führe Sie mit ENTER aus!")
+	firsted.Zeichne()
+	
 	//bubbletext.RahmenAn(true)
 	//bubbletext.SetzeRahmenFarbe(0,0,0)
+	bubbletext.HintergrundAn(true)
+	bubbletext.SetzeHintergrundFarbe(255,255,255)
 	bubbletext.SetzeZeilenAbstand(3)
 	bubbletext.SetzeSchriftgröße(18)
-	bubbletext.SchreibeText("\n\nUm die DBSA-Prüfung zu bestehen,\nmusst Du Dein SQL-Wissen nutzen,\num alle Geheimnisse der LWB-Welt\nzu enthüllen!")
+	bubbletext.SchreibeText(texte[0])
 	bubbletext.Zeichne()
 	bubblehead.SchreibeText("Willkommen zum SQL-Quest!")
 	bubblehead.Zeichne()
 	
-	
+	next.SetzeFont(path2 + "Schriftarten/Ubuntu-B.ttf")
+	next.ZeichneButton()
 	
 //----------------Eingabe-Verarbeitung----------------------------
-	ted = texteditoren.New(315,440,830,218,20,true)
+
+	//ted = texteditoren.New(315,440,830,218,20,true)
 	
-	if angeklickt(weiter) {
-		bubbletext.SchreibeText("neuerText")
+//----------------Spiel-Steuerung-----------------------------------
+
+	for {
+		taste, status, mausX, mausY := MausLesen1()
+		if taste==1 && status==1 {
+			if next.TesteXYPosInButton(mausX,mausY) {
+				//fmt.Println("Weiter angeklickt!")
+				bubbletext.SchreibeText(texte[aktuellerText])
+				bubbletext.Zeichne()
+				next.ZeichneButton()
+				if aktuellerText < len(texte)-1 {
+					aktuellerText++
+				}
+				
+				//----------------Eingabe-Verarbeitung--------------
+				ted = texteditoren.New(315,440,830,218,20,true)
+					ted.GibString()
+					//for {
+						//switch ted.GibString() {
+							//case
+			}
+		}
 	}
-	
-	ted.GibString()
-	//for {
-		//switch ted.GibString() {
-			//case 
-	
-	
+		
+
 	TastaturLesen1()
 	
 }
