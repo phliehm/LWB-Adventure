c7d4872d6f6ac5b76318c05b4a312d8bca3d483a
hseiss <herr.seiss@gmx.de>
2023-06-09 12:12:38 +0200

    Klasse Buttons bereinigt!

--- Klassen/buttons/buttons.go
+++ Klassen/buttons/buttons.go
@@ -1,20 +1,26 @@
-// ADT button - Spezifikation und Implementation
+// ADT button - Spezifikation
 
-// Martin Seiß    29.3.2023
+// Martin Seiß    29.3.2023 (Start)
 
-package buttons
-
-import "gfx"
-// import "fmt"
-
-// Buttons haben recheckige From.
+// Buttons haben recheckige Form.
+// werden.
 // x,y - bezeichet die obere linke Ecke.
 // b,h - die Breite und Höhe
 // r,g,b - Hintergrundfarbwerte des Buttons
 // aktiv - Button ist aktiv und bereit zur Positionsbestimmung
 // beschriftung	- String mit Beschriftung des Buttons
+// font - Font mit dem der Button beschriftet wird
+// sound - wav-Dateianame mit Pfad als String für Klang bei Tastendruck
 
-// New(x,y,h,br,r,g,b,aktiv,beschriftung) 
+// --> ACHTUNG: Vor dem ersten Aufrufen muss der Font einmal
+//				gesetzt worden sein!
+//				z.B: gfx.SetzeFont ("Schriftarten/Ubuntu-B.ttf",20)
+// New(x,y,bb,hh,r,g,b,aktiv,beschriftung) 
+
+package buttons
+
+// ------------  importierte Pakete und Klassen  -----------------//
+// import "gfx"
 
 
 type Button interface {
@@ -54,8 +60,8 @@ type Button interface {
 	SetzeFont(font string)
 
 	// Vor: -
-	// Eff: Der angegebene Sound wird verwendet, wenn Button aktiv und
-	//		getroffen.
+	// Eff: Der angegebene Sound-String wird verwendet, und erklingt,
+	//		wenn Button aktiv und getroffen.
 	// Erg: -
 	SetzeSound(sound string)
 
@@ -65,106 +71,4 @@ type Button interface {
 	// Erg: -
 	ZeichneButton()
 
-}
-
-
-
-type data struct {
-	x,y 		uint16		// bezeichet die obere linke Ecke.
-	br,h 		uint16		// die Breite und Höhe des Button
-	r,g,b 		uint8		// Hintergrundfarbwerte des Buttons
-	aktiv		bool		// true = Button aktiv
-	beschriftung 	string	// String mit Beschriftung des Buttons
-	font			string	// Name mit Pfad von Font
-	sound			string  // Name mit Pfad von Sound
-}
-
-
-
-func New(x,y,br,h uint16, r,g,b uint8, aktiv bool, beschriftung string) *data {
-	var but *data = new(data)
-	but.x = x
-	but.y = y
-	but.h = h
-	but.br = br
-	but.r = r
-	but.g = g
-	but.b = b
-	but.aktiv = aktiv
-	but.beschriftung = beschriftung
-	but.font = gfx.GibFont() 							//--> ACHTUNG: muss vorher einmal gesetzt worden sein!!! (Spez.)
-	but.sound = ""
-	return but
-}
-
-
-
-func (but *data) TesteXYPosInButton(x,y uint16) bool {
-	if but.aktiv {
-		if but.x <= x &&  x <= but.x+but.br {
-			if but.y <= y &&  y <= but.y+but.h{
-				if but.sound != "" {gfx.SpieleSound(but.sound)}
-				return true
-			}
-		} 
-	}
-	return false
-}
-
-
-
-func (but *data) AktiviereButton() {
-	but.aktiv = true
-}
-
-	
-	
-func (but *data) DeaktiviereButton() {
-	but.aktiv = false
-}
-	
-	
-func (but *data) GibAktivitaetButton() bool {
-	return but.aktiv
-}
-
-	
-func (but *data) AendereBeschriftung(text string) {
-	but.beschriftung = text
-}	
-	
-	
-	
-func (but *data) SetzeFont(font string) {
-	but.font = font
-}
-
-
-func (but *data) SetzeSound(sound string) {
-	but.sound = sound
-}
-	
-	
-func (but *data) ZeichneButton() {
-	var diff uint16 = 2
-	var fsize int = int(but.h/2)
-	
-	gfx.SetzeFont(but.font,fsize)
-
-	if but.aktiv {
-		gfx.Stiftfarbe(but.r,but.g,but.b)
-		gfx.Vollrechteck(but.x,but.y,but.br,but.h)
-		gfx.Stiftfarbe(0,0,0)
-		gfx.Rechteck(but.x,but.y,but.br,but.h)
-		gfx.Rechteck(but.x+diff,but.y+diff,but.br-2*diff,but.h-2*diff)
-		gfx.SchreibeFont(but.x+10,but.y+but.h/2-uint16(fsize)/2,but.beschriftung)
-	} else {
-		gfx.Stiftfarbe(but.r/2,but.g/2,but.b/2)
-		gfx.Vollrechteck(but.x,but.y,but.br,but.h)
-		gfx.Stiftfarbe(100,100,100)
-		gfx.Rechteck(but.x,but.y,but.br,but.h)
-		gfx.Rechteck(but.x+diff,but.y+diff,but.br-2*diff,but.h-2*diff)
-		gfx.SchreibeFont(but.x+10,but.y+but.h/2-uint16(fsize)/2,but.beschriftung)		
-		gfx.Stiftfarbe(0,0,0)
-	}
 }
