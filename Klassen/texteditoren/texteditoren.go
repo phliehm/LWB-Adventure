a567c945e9e358a8a261ca70edb64b1f39c5afb4
cyrianna <125969887+cyrianna@users.noreply.github.com>
2023-06-12 08:24:29 +0200

    hab meine Ordner aufgeräumt

--- Klassen/texteditoren/texteditoren.go
+++ Klassen/texteditoren/texteditoren.go
@@ -3,34 +3,71 @@
 //Klasse Texteditoren
 //basierend auf dem Quelltext von Stefan Schmidt (LWB Informatik, ALP 3)
 
+// Der ADT Texteditoren dient der Verwaltung von Vierecken. Jedes Viereck
+// hat vier Koordinaten, nämlich (xA,yA), (xB,yB), (xC,yC) und (xD,yD)
+// durch die es festgelegt wird. A ist dabei die obere linke Ecke.
+
+// Vor.: Soll der Texteditor vollständig im gfx-Fenster sichtbar sein,
+//       muss ein entsprechendes Fenster geöffnet sein oder die Maße
+//       in ein Fenster der Größe 1200x700 hineinpassen. 
+// Erg.: Ist kein gfx-Fenster offen, ist ein neues geöffnet (1200x700)
+//       ein Texteditor mit der übergebenen Breite und Höhe ist geliefert,
+//       dessen obere linke Ecke an den Koordinaten (posx,posy) liegt.
+//       Die Schriftart des Editors ist TerminusTTF-Bold-4.49.2 und die
+//       Schriftgröße entspricht dem übergebenen Wert. Wurde true übergeben,
+//       ist der Texteditor einzeilig, wurde false übergeben, sind
+//       Zeilenumbrüche mithilfe der Enter-Taste möglich.
+// New(posx,posy,breite,höhe uint16, schriftgr int, einzeilig bool) *data // *data erfüllt das Interface Texteditor
+
 package texteditoren
 
 type Texteditor interface {
 	
+	// Vor.: -
+	// Erg.: Die Koordinaten der oberen linken Ecke des Texteditors
+	//       sind geliefert.
 	GibPosition() (x,y uint16) 
 	
+	// Vor.: -
+	// Eff.: Die Koordinaten der oberen linken Ecke des Texteditors
+	//       sind entsprechend der übergebenen Werte neu gesetzt.
 	SetzePosition(xneu,yneu uint16)
 	
-	//GibFont() (font string)
-	
-	//SetzeFont(fontneu string)
-	
+	// Vor.: -
+	// Erg.: Die Höhe des Texteditors ist geliefert.
 	GibHöhe() (höhe uint16)
 	
+	// Vor.: -
+	// Eff.: Die Höhe des Texteditors ist entsprechend des übergebenen
+	//       Werts neu gesetzt.
 	SetzeHöhe(höheneu uint16)
 	
+	// Vor.: -
+	// Erg.: Die Breite des Texteditors ist geliefert.
 	GibBreite() (breite uint16)
 	
+	// Vor.: -
+	// Eff.: Die Breite des Texteditors ist entsprechend des übergebenen
+	//       Werts neu gesetzt.
 	SetzeBreite(breiteneu uint16)
 	
+	// Vor.: -
+	// Erg.: Die Schriftgröße des Texteditors ist geliefert.
 	GibSchriftgröße() (schriftgr int)
 	
+	// Vor.: -
+	// Eff.: Die Schriftgröße des Texteditors ist entsprechend des
+	//       übergebenen Werts neu gesetzt.
 	SetzeSchriftgröße(schriftgrneu int)
 	
+	// Vor.: -
+	// Erg.: Ist der Texteditor einzeilig, ist true geliefert,
+	//       andernfalls false.
 	IstEinzeilig() bool
 	
-	//NeuerTexteditor(posx,posy,breite,höhe uint16, schriftgr int, einzeilig bool) Texteditor
-	
+	// Vor.: -
+	// Erg.: Der in den Texteditor eingegebene Text ist als string geliefert.
+	//       Wurde nichts eingegeben, ist ein leerer String geliefert.	
 	GibString() string
 	
 }
--- Klassen/texteditoren/texteditorenImpl.go
+++ Klassen/texteditoren/texteditorenImpl.go
@@ -18,6 +18,16 @@ type data struct {
 	einzeilig bool
 }
 
+// Vor.: Soll der Texteditor vollständig im gfx-Fenster sichtbar sein,
+//       muss ein entsprechendes Fenster geöffnet sein oder die Maße
+//       in ein Fenster der Größe 1200x700 hineinpassen. 
+// Erg.: Ist kein gfx-Fenster offen, ist ein neues geöffnet (1200x700)
+//       ein Texteditor mit der übergebenen Breite und Höhe ist geliefert,
+//       dessen obere linke Ecke an den Koordinaten (posx,posy) liegt.
+//       Die Schriftart des Editors ist TerminusTTF-Bold-4.49.2 und die
+//       Schriftgröße entspricht dem übergebenen Wert. Wurde true übergeben,
+//       ist der Texteditor einzeilig, wurde false übergeben, sind
+//       Zeilenumbrüche mithilfe der Enter-Taste möglich.
 func New(posx,posy,breite,höhe uint16, schriftgr int, einzeilig bool) *data {
 	var ted *data
 	ted = new(data)
@@ -40,49 +50,71 @@ func New(posx,posy,breite,höhe uint16, schriftgr int, einzeilig bool) *data {
 
 //-------------------------- Methoden ---------------------------- 
 
+// Vor.: -
+// Erg.: Die Koordinaten der oberen linken Ecke des Texteditors
+//       sind geliefert.
 func (ted *data) GibPosition() (x,y uint16) {
 	return ted.x, ted.y
 }
 
+// Vor.: -
+// Eff.: Die Koordinaten der oberen linken Ecke des Texteditors
+//       sind entsprechend der übergebenen Werte neu gesetzt.
 func (ted *data) SetzePosition(xneu,yneu uint16) {
 	ted.x = xneu
 	ted.y = yneu
 }
 
-func (ted *data) GibSchriftgröße() (schriftgr int) {
-	return ted.schriftgr
-}
-
-func (ted *data) SetzeSchriftgröße(schriftgrneu int) {
-	ted.schriftgr = schriftgrneu
-}
-
+// Vor.: -
+// Erg.: Die Höhe des Texteditors ist geliefert.
 func (ted *data) GibHöhe() (höhe uint16) {
 	return ted.höhe
 }
 
+// Vor.: -
+// Eff.: Die Höhe des Texteditors ist entsprechend des übergebenen
+//       Werts neu gesetzt.
 func (ted *data) SetzeHöhe(höheneu uint16) {
 	ted.höhe = höheneu
 }
 
+
+// Vor.: -
+// Erg.: Die Breite des Texteditors ist geliefert.
 func (ted *data) GibBreite() (breite uint16) {
 	return ted.breite
 }
 
+// Vor.: -
+// Eff.: Die Breite des Texteditors ist entsprechend des übergebenen
+//       Werts neu gesetzt.
 func (ted *data) SetzeBreite(breiteneu uint16) {
 	ted.breite = breiteneu
 }
 
+// Vor.: -
+// Erg.: Die Schriftgröße des Texteditors ist geliefert.
+func (ted *data) GibSchriftgröße() (schriftgr int) {
+	return ted.schriftgr
+}
+
+// Vor.: -
+// Eff.: Die Schriftgröße des Texteditors ist entsprechend des
+//       übergebenen Werts neu gesetzt.
+func (ted *data) SetzeSchriftgröße(schriftgrneu int) {
+	ted.schriftgr = schriftgrneu
+}
+
+// Vor.: -
+// Erg.: Ist der Texteditor einzeilig, ist true geliefert,
+//       andernfalls false.
 func (ted *data) IstEinzeilig() bool {
 	return ted.einzeilig
 }
 
-/*
-func (ted *data) NeuerTexteditor(posx,posy,breite,höhe uint16, schriftgr int, einzeilig bool) *data {
-	ted = New(posx,posy,breite,höhe,schriftgr,einzeilig)
-}
-*/
-
+// Vor.: -
+// Erg.: Der in den Texteditor eingegebene Text ist als string geliefert.
+//       Wurde nichts eingegeben, ist ein leerer String geliefert.
 func (ted *data) GibString() string {
 	return ted.text
 }
